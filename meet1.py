64830e6f1718a727ab3937f6e8d1eca039018346
Mayzihardiputra <eziee496@gmail.com>
2025-10-07 14:54:21 +0700

    very good

--- meet1.py
+++ meet1.py
@@ -3,3 +3,6 @@ percentage = float(input("Masukkan persentase nilai siswa: "))
 if percentage >= 90:
     print("Excellent performance ")
 
+elif percentage >= 80:
+    print("Very Good Performance")
+
